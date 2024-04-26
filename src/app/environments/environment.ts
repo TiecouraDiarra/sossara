eba14b925d7b516861b44cd5b13e2ff12f36765c
Camara <camaramamady9160@gmail.com>
2024-04-26 10:46:04 +0000

    commite du vendredi matin

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -6,11 +6,11 @@ export const environment = {
     production: false,
     // Url_BASE:"https://api.sossara.ml/api",
     // Url_PHOTO:"https://api.sossara.ml"
-    // Url_BASE:"http://127.0.0.1:8080/api",
-    // Url_PHOTO:"http://127.0.0.1:8080",
-    // Url_PHOTO:"http://127.0.0.1:8080"
-    Url_BASE:"http://10.175.48.235:8080/api",
-    Url_PHOTO:"http://10.175.48.235:8080",
+    Url_BASE:"http://127.0.0.1:8080/api",
+    Url_PHOTO:"http://127.0.0.1:8080",
+  
+    // Url_BASE:"http://10.175.48.235:8080/api",
+    // Url_PHOTO:"http://10.175.48.235:8080",
   };
   
   /*
