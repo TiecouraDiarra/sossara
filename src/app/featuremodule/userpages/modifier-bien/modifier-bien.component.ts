c0c36dd8ace12f184968d4d0be59dc91a3195a12
Camara <camaramamady9160@gmail.com>
2024-06-05 12:12:51 +0000

    avant de prendre la branche de tiec

--- src/app/featuremodule/userpages/modifier-bien/modifier-bien.component.ts
+++ src/app/featuremodule/userpages/modifier-bien/modifier-bien.component.ts
@@ -726,7 +726,6 @@ changeExtension(filename: string, newExtension: string): string {
       });
     });
   }
-
   onKeyPress(event: any) {
     const pattern = /[0-9\ \+\-]/;
     let inputChar = String.fromCharCode(event.charCode);
@@ -737,17 +736,18 @@ changeExtension(filename: string, newExtension: string): string {
     }
   
     // Insérer un espace après chaque trio de chiffres
-    let inputValue = event.target.value.replace(/\s/g, ''); // Supprimer les espaces existants
-    let formattedValue = '';
-    for (let i = inputValue.length; i > 0; i -= 3) {
-      formattedValue = ',' + inputValue.slice(Math.max(i - 3, 0), i) + formattedValue;
-    }
-    // Supprimer l'espace initial s'il dépasse la limite de 1000 caractères
-    formattedValue = formattedValue.slice(0, 1000);
+    // let inputValue = event.target.value.replace(/\s/g, ''); // Supprimer les espaces existants
+    // let formattedValue = '';
+    // for (let i = inputValue.length; i > 0; i -= 3) {
+    //   formattedValue = ',' + inputValue.slice(Math.max(i - 3, 0), i) + formattedValue;
+    // }
+    // // Supprimer l'espace initial s'il dépasse la limite de 1000 caractères
+    // formattedValue = formattedValue.slice(0, 1000);
   
-    // Mettre à jour la valeur dans l'input
-    event.target.value = formattedValue;
+    // // Mettre à jour la valeur dans l'input
+    // event.target.value = formattedValue;
   }
+  
 
   quillConfig = {
     toolbar: {
