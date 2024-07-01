9f8c47e2f6a157aa56c78520081842c589f88c1c
Camara <camara@localhost.localdomain>
2024-07-01 13:42:13 +0000

    apres les merges

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -8,14 +8,13 @@ export const environment = {
     // Url_PHOTO:"https://api.sossara.ml",
     // socketUrl: 'https://api.sossara.ml/chat-socket'
 
-    Url_BASE:"http://127.0.0.1:8080/api",
-    Url_PHOTO:"http://127.0.0.1:8080",
-    socketUrl: '//127.0.0.1:8080/chat-socket'
+    // Url_BASE:"http://127.0.0.1:8080/api",
+    // Url_PHOTO:"http://127.0.0.1:8080",
+    // socketUrl: '//127.0.0.1:8080/chat-socket'
 
-    // Url_BASE:"http://10.175.48.169:8080/api",
-    // Url_PHOTO:"http://10.175.48.169:8080",
-    // socketUrl: 'http://10.175.48.169:8080/chat-socket'
-    
+    Url_BASE:"http://10.175.48.169:8084/api",
+    Url_PHOTO:"http://10.175.48.169:8084",
+    socketUrl: 'http://10.175.48.169:8084/chat-socket'
   };
   
   /*
